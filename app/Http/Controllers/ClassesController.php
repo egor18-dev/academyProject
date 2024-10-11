7f9252dbcdabec75e4446e04c610e2269bd3a800
mvindel_dev <marcvindeldev@gmail.com>
2024-10-11 17:21:02 +0200

    crear clases

--- app/Http/Controllers/ClassesController.php
+++ app/Http/Controllers/ClassesController.php
@@ -5,12 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ClassModel;
 use App\Models\Level;
+use Illuminate\Support\Facades\Validator;
 
 class ClassesController extends Controller
 {
     public function retrieveClasses(){
         $classes = ClassModel::paginate(10);
         return view('classes/view_classes', ['classes' => $classes]);
+    }
+    //display create class
+    public function dashboardCreateClass(){
+        $levels = Level::all();
+        return view('classes/add_class', ['levels' => $levels]);
+    }
+    //create the class
+    public function addClass(Request $request){
+        $validator = Validator::make($request->all(),[
+            'title' => 'required',
+            'level' => 'required',
+            'videoLink' => 'required|url',
+        ], [
+            'title.required' => 'El título es obligatorio.',
+            'level.required' => 'El nivel es obligatorio.',
+            'videoLink.required' => 'El vídeo es obligatorio.',
+            'videoLink.url' => 'Debes ingresar una URL válida.',
+        ]);
+
+        if($validator->fails()){
+            return back()->withInput()->withErrors($validator);
+        }
+
+        $levelParts = explode(' ', $request->level);
+        $levelId = end($levelParts);
+
+        ClassModel::create([
+            'title'=> $request->title,
+            'level_id'=> $levelId,
+            'video_url'=> $request->videoLink
+        ]);
+
+        return redirect()->to('classes');
     }
 
     public function updateClass(){
@@ -21,8 +55,4 @@ class ClassesController extends Controller
 
     }
 
-    public function dashboardCreateClass(){
-        $levels = Level::all();
-        return view('classes/add_class', ['levels' => $levels]);
-    }
 }
--- resources/views/classes/add_class.blade.php
+++ resources/views/classes/add_class.blade.php
@@ -1,3 +1,49 @@
+
 @extends('page')
 @section('page')
+    <main class="container-fluid mt-3">
+      <div class="row w-100">
+        <div class="col-12 col-md-11 col-lg-8 col-xl-7 col-xxl-12">
+          <div class="bg-white p-4 p-md-5 shadow-sm">
+            <div class="row w-100">
+              <div class="col-12">
+                <div class="mb-5">
+                  <h2 class="h3">Crear clase</h2>
+                  <h3 class="fs-6 fw-normal text-secondary m-0">Introduce toda la información</h3>
+                </div>
+              </div>
+            </div>  
+            <form action="{{ route('add-class') }}" method="POST">
+              @csrf 
+              <div class="row gy-3 gy-md-4 overflow-hidden">
+                <div class="col-12">
+                  <label for="title" class="form-label">Título <span class="text-danger">*</span></label>
+                  <input type="text" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" name="title" id="title" value="{{ old('title') }}" placeholder="Título de la clase">
+                </div>
+        
+                <div class="col-12">
+                  <label for="level" class="form-label">Nivel <span class="text-danger">*</span></label>
+                  <select class="form-select" name="level" id="level">
+                        @foreach ($levels as $level)
+                            <option value="{{ $level->name }}">{{ $level->name }}</option>
+                        @endforeach
+                  </select>
+                </div>
+
+                <div class="col-12">
+                  <label for="videoLink" class="form-label">Enlace del vídeo <span class="text-danger">*</span></label>
+                  <input type="text" class="form-control {{ $errors->has('videoLink') ? 'is-invalid' : ''  }}" name="videoLink" id="videoLink" value="{{ old('videoLink') }}">
+                </div>
+                
+                <div class="col-12">
+                  <div class="d-grid">
+                    <button class="principalBtn" type="submit">Crear clase</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+      </div>
+  </div>
+    </main>
 @endsection
