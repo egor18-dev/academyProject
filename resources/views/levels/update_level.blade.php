b78bfacf97cd7830b7b1b941b0e1d92668d96ead
Egor <egsrapps@gmail.com>
2024-10-19 20:24:33 +0200

    Update sembla

--- resources/views/levels/update_level.blade.php
+++ resources/views/levels/update_level.blade.php
@@ -6,54 +6,29 @@
           <div class="card custom-card">
               <div class="card-header justify-content-between">
                   <div class="card-title">
-                      Actualizar Usuario
+                      Actualizar Nivel
                   </div>
               </div>
               <div class="card-body">
-                  <form action="{{ route('users.update', ['uuid' => $user->uuid]) }}" method="POST" class="row g-3 needs-validation" novalidate>
+                  <form action="{{ route('users.update', ['uuid' => $level->uuid]) }}" method="POST" class="row g-3 needs-validation" novalidate>
                       @csrf 
                       @method('PUT')
-                      <div class="col-md-6">
+                      <div class="col-md-12">
                           <label for="name" class="form-label">Nombre <span class="text-danger">*</span></label>
-                          <input type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" name="name" id="name" value="{{ old('name', $user->name) }}" placeholder="Nombre del usuario" required>
+                          <input type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}" name="name" id="name" value="{{ old('name', $level->name) }}" placeholder="Descripción del nivel" required>
                           <div class="invalid-feedback">
                               Por favor, proporciona un nombre válido.
                           </div>
                       </div>
-                      <div class="col-md-6">
-                          <label for="surnames" class="form-label">Apellidos <span class="text-danger">*</span></label>
-                          <input type="text" class="form-control {{ $errors->has('surnames') ? 'is-invalid' : '' }}" name="surnames" id="surnames" value="{{ old('surnames', $user->surnames) }}" placeholder="Apellido del usuario" required>
+                      <div class="col-md-12">
+                          <label for="description" class="form-label">Apellidos <span class="text-danger">*</span></label>
+                          <textarea class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}" name="description" id="description" placeholder="Descripción del nivel" required>{{ old('description', $level->description) }}</textarea>
                           <div class="invalid-feedback">
                               Por favor, proporciona apellidos válidos.
                           </div>
                       </div>
-                      <div class="col-md-6">
-                          <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
-                          <input type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="email" id="email" value="{{ old('email', $user->email) }}" placeholder="nombre@ejemplo.com" required>
-                          <div class="invalid-feedback">
-                              Por favor, proporciona un correo electrónico válido.
-                          </div>
-                      </div>
-                      <div class="col-md-6">
-                          <label for="role" class="form-label">Función <span class="text-danger">*</span></label>
-                          <select class="form-select" name="role" id="role" required>
-                              @foreach ($roles as $role)
-                                  <option value="{{ $role->name }}" {{ $role->name === $actualUserRole ? 'selected' : '' }}>{{ $role->name }}</option>
-                              @endforeach
-                          </select>
-                          <div class="invalid-feedback">
-                              Por favor, selecciona una función.
-                          </div>
-                      </div>
-                      <div class="col-md-6">
-                          <label for="password" class="form-label">Contraseña <span class="text-danger">*</span></label>
-                          <input type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password" id="password" value="{{ old('password') }}" required>
-                          <div class="invalid-feedback">
-                              Por favor, proporciona una contraseña.
-                          </div>
-                      </div>
                       <div class="col-12">
-                          <button class="btn btn-primary" type="submit">Actualizar cuenta</button>
+                          <button class="btn btn-primary" type="submit">Actualizar nivel</button>
                       </div>
                   </form>
               </div>
