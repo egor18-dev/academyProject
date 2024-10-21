99f564a4028f38a1660c060aa72e87f2efbf66dd
Egor <egsrapps@gmail.com>
2024-10-21 15:25:31 +0200

    Comments

--- resources/views/classes/view_class.blade.php
+++ resources/views/classes/view_class.blade.php
@@ -994,13 +994,61 @@
     </div>
 
 </header>
-    <div class="row w-100">
+    <div class="row w-100 min-vh-100 bg-white">
         <div class="col-lg-9 d-flex flex-column videos">
             @if($class->video_stream)
                     <video controls loop class="rounded">
                         <source src="{{ route('classes.stream', ['id' => $class->id]) }}" type="video/mp4">
                         Tu navegador no soporta la reproducción de videos.
                     </video>
+                    <div class="info p-3">
+                        <h5>{{$class->title}}</h5>
+                        <p>
+                            {{$class->description}}
+                        </p>
+                    </div>
+                        <div class="container-fluid">
+                          <div class="row d-flex justify-content-start">
+                            <div class="col-md-12 col-lg-10 col-xl-8">
+                              <div class="card">
+                                <div class="card-body">
+                                  <div class="d-flex flex-start align-items-center">
+                                    <img class="rounded-circle shadow-1-strong me-3"
+                                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXA-Uu5DzOUC3DEEh789elx46nvfe-0s-7xg&s" alt="avatar" width="60"
+                                      height="60" />
+                                    <div>
+                                      <h6 class="fw-bold text-primary mb-1">Egor Santamaria</h6>
+                                      <p class="text-muted small mb-0">
+                                        21/10/2024
+                                      </p>
+                                    </div>
+                                  </div>
+                      
+                                  <p class="mt-3 mb-4 pb-2">
+                                    ¡He aprendido muchísimo sobre trading y mercados financieros! Ahora entiendo mejor cómo funcionan y cómo operar diferentes tipos de activos. ¡Recomendado!
+                                  </p>
+                                </div>
+                                <div class="card-footer border-0" style="background-color: #f8f9fa;">
+                                  <div class="d-flex flex-start w-100">
+                                    <img class="rounded-circle shadow-1-strong me-3"
+                                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXA-Uu5DzOUC3DEEh789elx46nvfe-0s-7xg&s" alt="avatar" width="40"
+                                      height="40" />
+                                    <div class="form-outline w-100">
+                                      <textarea class="form-control" id="textAreaExample" rows="4"
+                                        style="background: #fff;"></textarea>
+                                      <label class="form-label" for="textAreaExample">Message</label>
+                                    </div>
+                                  </div>
+                                  <div class="float-end">
+                                    <button type="button" class="btn btn-primary btn-sm">Post comment</button>
+                                    <button type="button" class="btn btn-outline-primary btn-sm">Cancel</button>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      
             @endif
         </div>
 </div>
