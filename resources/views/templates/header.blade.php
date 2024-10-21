938d16dc74be3a3466ddd5e5f684781981791209
Egor <egsrapps@gmail.com>
2024-10-21 16:50:43 +0200

    New Header

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -21,7 +21,7 @@
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
 
-      <a class="btn-getstarted" href="{{ route('sign-up') }}">Empezar</a>
+      <a class="btn-getstarted" href="{{ route('users.create') }}">Empezar</a>
 
     </div>
   </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,6 @@
     <!--BOOTSTRAP-->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <!--JQUERY-->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
