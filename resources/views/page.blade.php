36f59bed24b58df1dba1e5a6413e4fb7a083ab97
Egor <egsrapps@gmail.com>
2024-10-22 23:02:58 +0200

    Solution

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <section class="container-fluid">
         <div class="row w-100">
-            <div class="col-lg-2">
+            <div class="col-lg-2 d-flex align-items-start p-0">
                 @include('aside/aside')
             </div>
             <div class="col-lg-10 p-0 position-relative">
