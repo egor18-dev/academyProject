1e51a1a4452ea97076c894632e5a069ef9425d31
Egor <egsrapps@gmail.com>
2024-10-23 17:11:54 +0200

    Comments Model

---
+++ app/Models/Comments.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Support\Str;
+
+class Comments extends Model implements HasMedia
+{
+    use HasFactory, InteractsWithMedia;
+
+    protected $table = 'comments';
+
+    protected $fillable = ['uuid', 'user_id', 'class_id', 'description'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = (string) Str::uuid();
+        });
+    }
+}
