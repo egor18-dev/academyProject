ff36d01697970d4f2a1782350393510bc2e2c4d4
Egor <egsrapps@gmail.com>
2024-10-25 00:02:10 +0200

    Modification

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Exam extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['class_id', 'title'];
+    protected $fillable = ['level_id', 'title', 'description'];
 
     public function classModel()
     {
@@ -19,5 +20,14 @@ class Exam extends Model
     public function questions()
     {
         return $this->belongsToMany(Question::class, 'exam_question');
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = (string) Str::uuid();
+        });
     }
 }
